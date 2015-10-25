a3c25221086b8fd7bb7c6487cffb657f7aec4257
Ellen Thomson <ellenmthomson@gmail.com>
2015-10-25 18:56:37 -0500

    week9

---
+++ js/changing.js
@@ -0,0 +1,5 @@
+        //Ellen Thomson week9 webdev 2
+
+        $("document").ready(function () {
+          document.getElementById("changing").innerHTML = "Slow down, enjoy life. Keep it simple.";
+        });
